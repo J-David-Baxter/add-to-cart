d38ce50d17e54defac95de5cebbe563b1c9ee403
J-David-Baxter <jdavidbx@gmail.com>
2023-05-26 15:33:57 -0400

    Hook up app to firebase

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+const appSettings = {
+    databaseURL: "https://add-to-cart-2a286-default-rtdb.firebaseio.com/"
+};
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const itemsInDB = ref(database, "cartItems");
+
 const addButton = document.getElementById("add-button");
 const inputField = document.getElementById("input-field");
 
 addButton.addEventListener('click', () => {
     let inputValue = inputField.value;
-    console.log(inputValue);
+    push(itemsInDB, inputValue);
     
     inputField.value = '';
-})
+});
