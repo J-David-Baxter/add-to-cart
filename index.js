80d4c5fc4d6e948d5f94fa0eec35d0999db4926f
J-David-Baxter <jdavidbx@gmail.com>
2023-05-26 16:18:23 -0400

    Make render and input clear functions

--- index.js
+++ index.js
@@ -4,18 +4,26 @@ const appSettings = {
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
 import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
+function clearInputField(field) {
+    field.value = "";
+}
+
+function renderNewItem(newItem) {
+    cartList.innerHTML += `<li>${newItem}</li>`;
+}
+
 const app = initializeApp(appSettings);
 const database = getDatabase(app);
 const itemsInDB = ref(database, "cartItems");
 
 const addButton = document.getElementById("add-button");
 const inputField = document.getElementById("input-field");
-const itemList = document.getElementById('item-list');
+const cartList = document.getElementById('cart-list');
 
 addButton.addEventListener('click', () => {
     let inputValue = inputField.value;
     // push(itemsInDB, inputValue);
-    itemList.innerHTML += `<li>${inputValue}</li>`;
+    renderNewItem(inputValue);
     
-    inputField.value = '';
+    clearInputField(inputField);
 });
