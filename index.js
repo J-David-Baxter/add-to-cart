8d48be0b3a46cfba91ab75099bc5ecacab80a70f
J-David-Baxter <jdavidbx@gmail.com>
2023-05-26 15:05:20 -0400

    Style input and button, add image

---
+++ index.js
@@ -0,0 +1,9 @@
+const addButton = document.getElementById("add-button");
+const inputField = document.getElementById("input-field");
+
+addButton.addEventListener('click', () => {
+    let inputValue = inputField.value;
+    console.log(inputValue);
+    
+    inputField.value = '';
+})
