d05a686f9df5e4c65137d9e1d8d31dd3b9ea1066
J-David-Baxter <jdavidbx@gmail.com>
2023-05-29 19:57:54 -0400

    Remove comments

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebas
 import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 import { clearInputField, loadData } from "./utils.js";
 
-
-// Initialize app
 const appSettings = {
     databaseURL: "https://add-to-cart-2a286-default-rtdb.firebaseio.com/"
 };
@@ -11,22 +9,15 @@ const app = initializeApp(appSettings);
 export const database = getDatabase(app);
 const itemsInDB = ref(database, "cartItems");
 
-// HTML elements
 const addButton = document.getElementById("add-button");
 const inputField = document.getElementById("input-field");
 const cartList = document.getElementById("cart-list");
 
-// Load data from database
 loadData(itemsInDB, cartList);
 
-// Add new item to database
 addButton.addEventListener('click', () => {
     let inputValue = inputField.value.trim();
     if (inputValue) { push(itemsInDB, inputValue) };
     clearInputField(inputField);
     inputField.focus();
 });
-
-// inputField.addEventListener('keyup', () => {
-//     addButton.disabled = inputField.value.length === 0;
-// })
